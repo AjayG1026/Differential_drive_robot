34afa9f6049119c24ac3e294a5d372d216141fc7
AjayG1026 <ajaygudise03@gmail.com>
2026-03-09 10:43:42 +0530

    Wj

---
+++ build/bumperbot_msgs/bumperbot_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ajay/bumperbot_ws/build/bumperbot_msgs/bumperbot_msgs__py
+# Build directory: /home/ajay/bumperbot_ws/build/bumperbot_msgs/bumperbot_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
